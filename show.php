8a3eedee5c23aea8161efa6f90afd3422c301cfb
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-13 17:35:21 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLab', 3306);
+mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLap', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
