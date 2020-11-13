accc068ca8bbd42aa26dfe75de82e2aba7785676
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-13 20:42:43 +0700

    Update show.php

--- show.php
+++ show.php
@@ -26,7 +26,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    
+    <td><?php echo $Result[<input type="submit" value="เพิ่ม">];?></td>
   </tr>
 <?php
 }
