2f6fa76c84cea33404163bb8e4705337eca8e1fa
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-14 15:40:16 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+if($_REQUEST['del_id'] != "")
+{
+$del_id = $_REQUEST['del_id'];
+$sql = "DELETE FROM guestbook WHERE id=$del_id";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
