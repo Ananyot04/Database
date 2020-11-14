848be4d3ee93cec207aa494b003720493762f366
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-14 15:57:23 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,8 +8,7 @@ if (mysqli_connect_errno($conn))
 if($_REQUEST['del_id'] != "")
 {
 $del_id = $_REQUEST['del_id'];
-$sql = "DELETE FROM guestbook WHERE id=$del_id";
-$sql_del = "delete from customer where id = '$del_id';";
+$sql_del = "DELETE FROM guestbook WHERE id=$del_id";
 mysql_query($sql_del) or die(mysql_error());
 echo "ลบข้อมูล ID $del_id เรียบร้อยแล้ว";
 }
