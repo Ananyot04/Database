68cd6399b9c9982c7a8edef28ec274280c22705d
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-15 15:47:29 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $name = $_POST['Name'];
-$strSQL = "delete from guestbook where Name = '".$_GET["name"]."' ";
+$strSQL = "delete from guestbook where Name = '.$name.' ";
 $objQuery = mysqli_query($strSQL);
 if($objQuery) {
     echo "Delete successfully.";
