b3a7897bd04c215994ec38a05c2c8a11f6bc728d
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-15 14:39:48 +0700

    Update show.php

--- show.php
+++ show.php
@@ -27,8 +27,8 @@ while($Result = mysqli_fetch_array($res))
     <td><div align="center"><?php echo $Result['Name'];?></div></td>
     <td><div align="center"><?php echo $Result['Comment'];?></div></td>
       <td>
-          <div align="center"><a href="delete.php?name=".$row["Name"].">ลบ</a></div>
-              <div align="center"><a href="update.php?name=".$row["Name"].>แก้ไข</a></div></td>
+          <div align="center"><a href="delete.php?name=".$Result["Name"].">ลบ</a></div>
+              <div align="center"><a href="update.php?name=".$Result["Name"].">แก้ไข</a></div></td>
   </tr>
 <?php
 }
