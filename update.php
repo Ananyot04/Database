87ad72e4a2c1740395eb245be43be29b90c0ac6a
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-17 16:43:42 +0700

    Update update.php

--- update.php
+++ update.php
@@ -5,32 +5,11 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-    mysqli_query($conn,"UPDATE guestbook set name='" . $_POST['name'] . "', comment='" . $_POST['comment'] . "' WHERE name='" . $_POST['name'] . "'");
-    $message = "Record Modified Successfully";
-
-    $result = mysqli_query($conn,"SELECT * FROM guestbook WHERE name='" . $_GET['name'] . "'");
-    $row= mysqli_fetch_array($result);
-    ?>
-    <html>
-    <head>
-    <title>Update Employee Data</title>
-    </head>
-    <body>
-    <form name="frmUser" method="post" action="">
-    <div><?php if(isset($message)) { echo $message; } ?>
-    </div>
-    <div style="padding-bottom:5px;">
-    </div>
-    Name: <br>
-    <input type="hidden" name="name" class="txtField" value="<?php echo $row['Name']; ?>">
-    <input type="text" name="name"  value="<?php echo $row['Name']; ?>">
-    <br>
-    Comment: <br>
-    <input type="text" name="comment" class="txtField" value="<?php echo $row['Comment']; ?>">
-    <br>
-    <input type="submit" name="submit" value="Submit" class="buttom">
-    
-    </form>
-    </body>
-    </html>
+$sql = "update guestbook set name='".$_POST["Name"]."', comment='".$_POST["Comment"]."' where name='".$_POST["Name"];
+if(mysqli_query($conn, $sql)) {
+    echo "update successfully";
+} else {
+    echo "Error: " .$sql. "<br>". mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
