14af0b6179e22486fb6012d7556e235e85556533
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-17 15:40:05 +0700

    Update show.php

--- show.php
+++ show.php
@@ -15,7 +15,6 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table width="600" border="1">
   <tr>
-      <th width="50"><div align="center">ID</div></th>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Action </div></th>
@@ -25,12 +24,11 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-      <td><div align="center"><?php echo $Result["ID"];?></div></td>
     <td><div align="center"><?php echo $Result["Name"];?></div></td>
     <td><div align="center"><?php echo $Result["Comment"];?></div></td>
       <td>
-          <div align="center"><a href="delete.php?id=<?php echo $Result["ID"];?>">ลบ</a></div>
-              <div align="center"><a href="update.php?id=<?php echo $Result["ID"];?>">แก้ไข</a></div></td>
+          <div align="center"><a href="delete.php?name=<?php echo $Result["Name"];?>">ลบ</a></div>
+              <div align="center"><a href="update.php?name=<?php echo $Result["Name"];?>">แก้ไข</a></div></td>
   </tr>
 <?php
 }
