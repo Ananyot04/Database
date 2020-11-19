e682bc575bc93a231f770a524cc80afc4f184a75
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 17:38:07 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -5,17 +5,42 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<form method="post" action="server.php" >
+	<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+?>
+	<?php 
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($db, "SELECT * FROM guestbook WHERE id=$id");
+
+		if (count($record) == 1 ) {
+			$n = mysqli_fetch_array($record);
+			$name = $n['name'];
+			$comment = $n['comment'];
+		}
+	}
+?>
+	<form method="post" action="update.php" >
 		<div class="input-group">
 			<label>Name</label>
-			<input type="text" name="name" value="">
+			<input type="text" name="name" value="<?php echo $name; ?>">
 		</div>
 		<div class="input-group">
 			<label>Comment</label>
-			<input type="text" name="comment" value="">
+			<input type="text" name="comment" value="<?php echo $comment; ?>">
 		</div>
 		<div class="input-group">
-			<button class="btn" type="submit" name="save" >Save</button>
+			<?php if ($update == true): ?>
+	<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
+<?php else: ?>
+	<button class="btn" type="submit" name="save" >Save</button>
+<?php endif ?>
 		</div>
 	</form>
 </body>
