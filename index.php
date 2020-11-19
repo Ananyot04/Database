1bc0b6f7ba2b95f29c3efe43a905d6f250527fae
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 21:32:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,30 @@
 		?>
 	</div>
 <?php endif ?>
+	<?php $results = mysqli_query($db, "SELECT * FROM guestbook"); ?>
+
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Comment</th>
+			<th colspan="2">Action</th>
+		</tr>
+	</thead>
+	
+	<?php while ($row = mysqli_fetch_array($results)) { ?>
+		<tr>
+			<td><?php echo $row['name']; ?></td>
+			<td><?php echo $row['comment']; ?></td>
+			<td>
+				<a href="index.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
+			</td>
+			<td>
+				<a href="php_code.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
 	<form method="post" action="php_code.php" >
 		<div class="input-group">
 			<label>Name</label>
