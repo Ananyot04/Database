749295358f650d81ad755bf6d1dee50df4df733d
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 18:44:29 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -13,33 +13,31 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 ?>
-	<?php 
-	
-		$id = $_GET['Name'];
-		$record = mysqli_query($conn, "SELECT * FROM guestbook WHERE name=$id");
-
-		if (count($record) == 1 ) {
-			$n = mysqli_fetch_array($record);
-			$name = $n['Name'];
-			$comment = $n['Comment'];
-		}
-
-	
+	$name = $_POST['Name'];
+$sql = "SELECT * FROM guestbook WHERE name='$name'";
+$res = mysqli_query($conn, $sql);
+$comment = mysqli_fetch_array($res);
 ?>
-	<form method="post" action="update.php" >
-		<input type="hidden" name="id" value="<?php echo $id; ?>">
-		<div class="input-group">
-			<label>Name</label>
-			<input type="text" name="name" value="<?php echo $name; ?>">
-		</div>
-		<div class="input-group">
-			<label>Comment</label>
-			<input type="text" name="comment" value="<?php echo $comment; ?>">
-		</div>
-		<div class="input-group">
-			
-	<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
-		</div>
-	</form>
+    <div class="container">
+        <h1>Edit comment</h1>
+        <form action="update.php" method="post" class="mt-4">
+            <div class="form-group">
+                <label for="inputName">Name</label>
+                <?php
+                    echo '<input type="text" name="name" id="inputName" class="form-control" placeholder="Enter Name" value="'.$comment["Name"].'">'
+                ?>
+            </div>
+            <div class="form-group">
+                <label for="inputComment">Comment</label>
+                <textarea name="comment" class="form-control" id="inputComment" rows="3" placeholder="Enter Comment"><?php echo $comment['Comment'];?></textarea>
+            </div>
+            <div class="mt-4">
+                <button type="submit" class="btn btn-primary mr-1">update</button>
+            </div>
+        </form>
+    </div>
+<?php
+mysqli_close($conn);
+?>
 </body>
 </html>
