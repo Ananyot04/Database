ec2b6fe0601d23ec8d2a51d917351963e311c292
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 20:19:53 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,38 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Update</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	
-</head>
-<body>
-	<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-?>
-	<?php
-	$id = $_GET['Name'];
-	$record = mysqli_query($conn, "SELECT * FROM guestbook WHERE name=$id");
-	$res = mysqli_fetch_array($record);
-		
-	
-?>
-		<form method="post" action="update.php" >
-		<div class="input-group">
-			<label>Name</label>
-			<input type="text" name="name" value="<?php=$res["name"]?>">
-		</div>
-		<div class="input-group">
-			<label>Comment</label>
-			<input type="text" name="comment" value="<?php=$res["comment"]?>">
-		</div>
-		<div class="input-group">
-			<button class="btn" type="submit" name="save" >update</button>
-		</div>
-	</form>
-</body>
-</html>
+<?php
+    $conn = mysqli_connect('apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab');
+
+    $id = $_GET['Name'];
+
+    $sql = 'SELECT * FROM guestboook WHERE name = ' . $id . '';
+    $query = mysqli_query($conn, $sql);
+    if (!$query) {
+        header('Location: show.php');
+    } else {
+        $data = mysqli_fetch_assoc($query);
+    }
+    ?>
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+      <title>Comment Form</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  </head>
+
+  <body>
+      <div class="container">
+          <div class="card-header bg-primary text-white d-flex justify-content-between">
+           <h3>EDIT</h3>
+           <a href="index.php" class="btn btn-light">BACK</a>
+          </div>
+          <form action="update.php" method="post" id="CommentForm">
+              <div class="form-group mt-5">
+             
+                  <label class="m-3" for="name">Name</label>
+                  <input type="text" class="form-control" name="name" id="idName" value="<?php echo $data['Name'];?>">
+                  <label class="m-3" for="comment">Comment</label>
+                  <textarea rows="5" class="form-control" cols="20" name="comment" id="idComment" ><?php echo $data['Comment'];?></textarea><br>
+                  <input class="btn btn-primary mt-5" type="submit" id="commentBtn">
+              </div>
+          </form>
+      </div>
+  </body>
+
+  </html>
