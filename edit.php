3984e2b6c6f1219b8ebf526c64a0480a6631c9b1
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 20:27:05 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,5 +1,10 @@
 <?php
-    $conn = mysqli_connect('ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFlab', 3306);
+   $conn = mysqli_init();
+mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
     $id = $_GET['Name'];
 
