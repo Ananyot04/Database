86f7106429398c4692f5cc7a439e0788d3b15eb9
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 17:17:31 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,29 +1,21 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
 </head>
 <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'ananyot.mysql.database.azure.com', 'kilzen@ananyot', 'sM7wgvcaip', 'ITFLab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$sql = "SELECT * FROM guestbook where name='".$_GET["Name"]."'";
-$res = mysqli_query($conn, $sql);
-if(mysqli_num_rows($res) > 0) {
-    while ($row1 = mysqli_fetch_assoc($res)) {
-?>
-<form action="update.php" method="post">
-<table border="1">
-    <tr><td>Name</td><td><input type="text" name="name" value="<?php=$row1["Name"]?>"></td></tr>
-    <tr><td>Comment</td><td><input type="text" name="comment" value="<?php=$row1["Comment"]?>"></td></tr>
-    }
-}
-
-    </select></table>
-<input type="submit" name="Submit" value="submit">
-    </form>
+	<form method="post" action="server.php" >
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="">
+		</div>
+		<div class="input-group">
+			<label>Comment</label>
+			<input type="text" name="comment" value="">
+		</div>
+		<div class="input-group">
+			<button class="btn" type="submit" name="save" >Save</button>
+		</div>
+	</form>
 </body>
 </html>
