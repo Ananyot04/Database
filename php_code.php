f9ea4d81f8fc23aa409e5da17378b99949108168
Ananyot04 <73694354+Ananyot04@users.noreply.github.com>
2020-11-19 22:09:41 +0700

    Update php_code.php

--- php_code.php
+++ php_code.php
@@ -30,3 +30,10 @@ if (isset($_POST['update'])) {
 	$_SESSION['message'] = "Information updated!"; 
 	header('location: index.php');
 }
+if (isset($_GET['del'])) {
+	$id = $_GET['del'];
+	mysqli_query($db, "DELETE FROM info WHERE id=$id");
+	$_SESSION['message'] = "Address deleted!"; 
+	header('location: index.php');
+}
+?>
